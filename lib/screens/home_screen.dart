d12836c5e45eaeabbf14e28bb2227470ed5b04e6
yulibahamon <yulibahamon13@gmail.com>
2024-03-24 23:38:31 -0500

    cambios en login y register, mas crecion de componentes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,193 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:lemon_guard/screens/register_screen.dart';
-import 'package:lemon_guard/screens/users/edit_screen.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:lemon_guard/routes.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-class HomeScreen extends StatefulWidget {
+import 'package:lemon_guard/screens/base_style.dart';
+import 'package:lemon_guard/utils/validations.dart';
+import 'package:lemon_guard/utils/api_user_service.dart';
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.transparent,
+      body: BackgroundContainer(
+        child: HomeBody(), // Envuelve el contenido con BackgroundContainer
+      ),
+    );
+  }
+}
+class HomeBody extends StatefulWidget {
+  const HomeBody({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeBody> createState() => _HomeBodyState();
 }
-
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeBodyState extends State<HomeBody> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final String loginUrl = ApiService.apiLoginUrl;
-  bool isLoading = false;
-  final _formKey = GlobalKey<FormState>();
+
+  // Función para mostrar una advertencia
+  void showWarning(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/fondo.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 200),
-          child: Center(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.95,
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: Container(
-                padding: const EdgeInsets.only(top: 0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
+    return Padding(
+      padding: const EdgeInsets.only(top: 200),
+      child: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.95,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Container(
+            padding: const EdgeInsets.only(top: 0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      // Botones dentro del contenedor blanco redondeado
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 20),
-                              backgroundColor:
-                                  const Color.fromARGB(255, 53, 195, 18),
-                              foregroundColor: Colors.white,
-                              minimumSize: const Size(125, 50),
-                            ),
-                            child: const Text('Iniciar sesión'),
-                          ),
-                          const SizedBox(width: 15),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const RegisterScreen()),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 20),
-                              backgroundColor: const Color.fromARGB(
-                                  255, 124, 129, 122),
-                              foregroundColor: Colors.white,
-                              minimumSize: const Size(125, 50),
-                            ),
-                            child: const Text('Registrarse'),
-                          ),
-                        ],
+                      GreenRoundedButton( 
+                        text: 'Iniciar sesión',
+                        onPressed: () {},
                       ),
-                      const SizedBox(height: 20),
-                      RichText(
-                        text: const TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: "Bienvenido a LemonGuard ",
-                              style: TextStyle(
-                                  fontSize: 25, color: Colors.black),
-                            ),
-                          ],
+                      const SizedBox(width: 15),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                            MaterialPageRoute(
+                                builder: (context) =>const RegisterScreen()),
+                        ); },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20),
+                          backgroundColor: const Color.fromARGB(255, 124, 129, 122),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(125, 50),
                         ),
-                      ),
-                      const SizedBox(height: 40),
-                      TextFormField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          labelText: "Email",
-                          border: OutlineInputBorder(borderSide: BorderSide.none),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: "Contraseña",
-                          border: OutlineInputBorder(borderSide: BorderSide.none),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            // Validar email
-                            if (emailController.text.isEmpty || !emailValidator(emailController.text)) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text(
-                                        'Formato de Correo electrónico no válido.')),
-                              );
-                              return;
-                            }
-
-                            // Validar contraseña
-                            if (passwordController.text.isEmpty || passwordController.text.length < 8) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'La contraseña debe tener al menos 8 caracteres.'
-                                      )
-                                ),
-                              );
-                              return;
-                            }
-
-                            // Mostrar indicador de carga
-                            setState(() {
-                              isLoading = true;
-                            });
-                            final email = emailController.text;
-                            final password = passwordController.text;
-                            final body = {'email': email, 'password': password};
-                            print('datos: ${body}');
-                            try {
-                              final response = await http.post(
-                                Uri.parse(loginUrl),
-                                body: body,
-                              );
-
-                              // Manejar respuesta exitosa
-                              if (response.statusCode == 200) {
-                                jsonDecode(response.body);
-                              } else {
-                                // Manejar respuesta con error
-                                final errorMessage = jsonDecode(response.body)['error'];
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(errorMessage)),
-                                );
-                              }
-                            } catch (error) {
-                              // Manejar errores de red u otros
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(content: Text('Error al enviar el formulario.')),
-                              );
-                            } finally {
-                              // Ocultar indicador de carga
-                              setState(() {
-                                isLoading = false;
-                              });
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 20),
-                            backgroundColor:
-                                const Color.fromARGB(255, 53, 195, 18),
-                            foregroundColor: Colors.white,
-                            minimumSize: const Size(200, 50),
-                          ),
-                          child: const Text('Iniciar sesión'),
-                        ),
+                        child: const Text('Registrarse'),
                       ),
                     ],
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: const TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Bienvenido a LemonGuard ",
+                          style: TextStyle(fontSize: 25, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Contraseña",
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: GreenRoundedButton( 
+                      text: 'Iniciar sesión',
+                      onPressed: () {
+                        // Obtiene el valor del correo electrónico y la contraseña
+                        final email = emailController.text.trim();
+                        final password = passwordController.text.trim();
+
+                        // Valida el correo electrónico y la contraseña
+                        if (email.isEmpty || !ValidationUtils.isValidEmail(email)) {showWarning('Por favor, introduce un correo electrónico válido.');
+                          return;
+                        }
+                        if (password.isEmpty || !ValidationUtils.isValidPassword(password)) {showWarning('La contraseña debe tener al menos 8 caracteres.');
+                          return;
+                        }
+                        Login.login(email, password);
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -195,11 +131,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
-  bool emailValidator(String email) {
-  return RegExp(
-      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9]+\.[a-z]+")
-      .hasMatch(email);
 }
-}
-
